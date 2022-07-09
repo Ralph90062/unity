9653651bca2c63c1be2d710e0a2279e5297ed7f6
Ralph26 <ralph90062@gmail.com>
2022-07-09 15:23:49 -0700

    Unity

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagment;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void Scene1 () {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Scene2 () {
+        SceneManager.LoadScene("Music Menu");
+    }
+
+
+}   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ 
